f72da8785878d0af7c136ad241aa8627fc5e8b19
sast5859 <sami.strack@gmail.com>
2021-12-01 12:42:27 +0100

    router id

--- script.py
+++ script.py
@@ -10,21 +10,22 @@ def config_telnet():
     area_id = 0
     tn = telnetlib.Telnet(HOST)
     interfaces=[]
+    process_id= 3
     tn.read_until("Username: ")
     tn.write(user + "\n")
     tn.read_until("Password: ")
     if password:
         #on configure OSPF
-        #attention on construit le process id et router id sur le dernier octet de l'ip
+        #attention on construit le  router id sur le dernier octet de l'ip
         # si on a plusieurs sous reseau alors certains se retrouveront avec le meme router id
         tn.write(password + "\n")
         host_parse = HOST.split(".")
-        process_id = host_parse[3]
+        router_id_base = host_parse[3]
         tn.write("enable\n")
         tn.write("cisco\n")
         tn.write("configure t\n")
         tn.write("router ospf "+process_id)
-        router_id = process_id+"."+process_id+"."+process_id+"."+process_id
+        router_id = router_id_base+"."+router_id_base+"."+router_id_base+"."+router_id_base
         tn.write("router-id"+router_id)
         tn.write("end\n")
         #on active ospf
